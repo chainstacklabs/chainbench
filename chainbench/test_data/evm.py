7ff5c1cb86e761ab01d6931cdf964a83cc1533d0
Erwin Wee <116077511+erwin-wee@users.noreply.github.com>
2023-05-30 22:33:39 +0800

    fix issue with account addresses sometimes being null (#15)

--- chainbench/test_data/evm.py
+++ chainbench/test_data/evm.py
@@ -5,11 +5,17 @@ from chainbench.test_data.base import BaseTestData, BlockchainData
 
 class EVMTestData(BaseTestData):
     TXS_REQUIRED = 50
+    ACCOUNTS_REQUIRED = 100
     SAVE_LAST_BLOCKS = 100
 
     @staticmethod
     def _parse_hex_to_int(value: str) -> int:
         return int(value, 16)
+
+    @staticmethod
+    def _append_if_not_none(data_list, val):
+        if val is not None:
+            data_list.append(val)
 
     def _fetch_block(
         self, block_number: int | str, return_txs: bool = True
@@ -32,25 +38,25 @@ class EVMTestData(BaseTestData):
     # get initial data from blockchain
     def _get_init_data(self) -> BlockchainData:
         latest_block_number, latest_block = self._fetch_block("latest")
-        txs = []
-        tx_hashes = []
-        accounts = []
+        txs: list[dict] = []
+        tx_hashes: list[str] = []
+        accounts: list[str] = []
         blocks = [(latest_block_number, latest_block["hash"])]
         # extract data from block
         for tx in latest_block["transactions"]:
-            txs.append(tx)
-            tx_hashes.append(tx["hash"])
-            accounts.append(tx["from"])
-            accounts.append(tx["to"])
+            self._append_if_not_none(txs, tx)
+            self._append_if_not_none(tx_hashes, tx["hash"])
+            self._append_if_not_none(accounts, tx["from"])
+            self._append_if_not_none(accounts, tx["to"])
 
-        while self.TXS_REQUIRED > len(txs):
+        while self.TXS_REQUIRED > len(txs) and self.ACCOUNTS_REQUIRED > len(accounts):
             block_number, block = self._fetch_random_block(0, latest_block_number)
             blocks.append((block_number, block["hash"]))
             for tx in block["transactions"]:
-                txs.append(tx)
-                tx_hashes.append(tx["hash"])
-                accounts.append(tx["from"])
-                accounts.append(tx["to"])
+                self._append_if_not_none(txs, tx)
+                self._append_if_not_none(tx_hashes, tx["hash"])
+                self._append_if_not_none(accounts, tx["from"])
+                self._append_if_not_none(accounts, tx["to"])
 
         return BlockchainData(
             latest_block_number=latest_block_number,
