6e94d3fdddb806ccbb5731031c71956920ce1d50
Erwin Wee <erwin.wee@chainstack.com>
2023-06-30 18:26:00 +0800

    CORE-8300 add more heavy resource usage methods
    - added eth_getBlockReceipts, trace_replayTransaction, trace_replayBlockTransactions, trace_call, trace_filter, debug_traceCall, debug_storageRangeAt, debug_traceBlockByNumber, debug_traceBlockByHash, eth_estimateGas methods that potentially use a lot of resources
    - removed randomness from json rpc "id" and hardcode it to "1" so that when the same errors occur they can be grouped in locust instead of being considered unique errors.

--- chainbench/user/evm.py
+++ chainbench/user/evm.py
@@ -15,8 +15,10 @@ class EVMBenchUser(BaseBenchUser):
     def _get_logs_params_factory(self):
         return [
             {
-                "fromBlock": self.test_data.get_random_recent_block_number_hex(20),
-                "toBlock": "latest",
+                "fromBlock": hex(
+                    self.test_data.latest_block_number - random.randint(0, 20)
+                ),
+                "toBlock": hex(self.test_data.latest_block_number),
             }
         ]
 
@@ -39,4 +41,46 @@ class EVMBenchUser(BaseBenchUser):
         return [
             self.test_data.get_random_account(),
             self.test_data.get_random_block_number_hex(),
+        ]
+
+    def _trace_block_by_number_params_factory(self):
+        return [
+            self.test_data.get_random_block_number_hex(),
+            {"tracer": "callTracer"},
+        ]
+
+    def _trace_block_by_hash_params_factory(self):
+        return [
+            self.test_data.get_random_block_hash(),
+            {"tracer": "callTracer"},
+        ]
+
+    def _trace_replay_block_transaction_by_block_number_params_factory(self):
+        return [
+            self.test_data.get_random_block_number_hex(),
+            ["vmTrace", "trace", "stateDiff"],
+        ]
+
+    def _trace_replay_transaction_by_hash_params_factory(self):
+        return [self.test_data.get_random_tx_hash(), ["vmTrace", "trace", "stateDiff"]]
+
+    def _trace_transaction_by_hash_params_factory(self):
+        return [self.test_data.get_random_tx_hash(), {"tracer": "prestateTracer"}]
+
+    def _trace_filter_params_factory(self):
+        block_number = self.test_data.get_random_block_number()
+        return [
+            {
+                "fromAddress": [self.test_data.get_random_account()],
+                "fromBlock": hex(block_number),
+                "toBlock": hex(block_number + random.randint(0, 20)),
+            }
+        ]
+
+    def _eth_estimate_gas_params_factory(self):
+        return [
+            {
+                "from": self.test_data.get_random_account(),
+                "to": "0x18318221d811Da0fe45412394eAf2C42A10BC678",
+            }
         ]
