e64fc1684a7bd1c24f4fd09f07715c2407ec8a4d
Erwin Wee <erwin.wee@chainstack.com>
2023-07-21 12:54:44 +0700

    add support for solana
    - added test_data module for solana to generate solana test data
    - added user module for solana for creating rpc requests
    - added solana locust profile
    - updated dependencies

--- chainbench/test_data/__init__.py
+++ chainbench/test_data/__init__.py
@@ -1,9 +1,11 @@
 from .base import BaseTestData
 from .dummy import DummyTestData
 from .evm import EVMTestData
+from .solana import SolanaTestData
 
 __all__ = [
     "BaseTestData",
     "DummyTestData",
     "EVMTestData",
+    "SolanaTestData",
 ]
