55ba961d3e0b21cbd12b229f26675f75fd8f4936
Erwin Wee <erwin.wee@chainstack.com>
2023-07-21 12:53:19 +0700

    refactor test data generation
    - refactor test data generation to happen only once in the master process, and then use custom messaging to send test data to all workers.
    - remove duplicate code from monitor.py, use existing function from locust.util.timespan instead.

--- chainbench/util/event.py
+++ chainbench/util/event.py
@@ -17,6 +17,27 @@ def cli_custom_arguments(parser):
     )
 
 
+def setup_test_data(environment, msg, **kwargs):
+    # Fired when the worker receives a message of type 'test_data'
+    test_data = msg.data[0]
+    worker_index = msg.data[1]
+
+    for user in environment.runner.user_classes:
+        if not hasattr(user, "test_data"):
+            continue
+
+        user.test_data.init_data_from_json(test_data[user.__class__.__name__])
+    environment.runner.send_message(
+        "acknowledge_data", f"Test data received by worker {worker_index}"
+    )
+    logger.info("Test Data received from master")
+
+
+def on_acknowledge(msg, **kwargs):
+    # Fired when the master receives a message of type 'acknowledge_data'
+    print(msg.data)
+
+
 # Listener for the init event
 def on_init(environment, **_kwargs):
     # It will be called for any runner (master, worker, local)
@@ -26,26 +47,43 @@ def on_init(environment, **_kwargs):
 
     host_under_test = environment.host or "Default host"
 
-    if isinstance(environment.runner, MasterRunner):
-        # Print master details to the log
-        logger.info("I'm a master. Running tests for %s", host_under_test)
-
-    if isinstance(environment.runner, WorkerRunner):
+    if not isinstance(environment.runner, MasterRunner):
         # Print worker details to the log
         logger.info("I'm a worker. Running tests for %s", host_under_test)
+        environment.runner.register_message("test_data", setup_test_data)
+
+    if not isinstance(environment.runner, WorkerRunner):
+        # Print master details to the log
+        logger.info("I'm a master. Running tests for %s", host_under_test)
+        environment.runner.register_message("acknowledge_data", on_acknowledge)
+
+
+def on_test_start(environment, **_kwargs):
+    test_data = {}
+
+    # It will be called for any runner (master, worker, local)
+    if not isinstance(environment.runner, WorkerRunner):
         logger.info("Initializing test data...")
+        print("Initializing test data...\n")
         for user in environment.runner.user_classes:
             if not hasattr(user, "test_data"):
                 continue
 
-            user.test_data.update(
-                environment.host, environment.parsed_options.use_recent_blocks
-            )
-            logger.info("Test data is ready")
+            if user.__class__.__name__ not in test_data:
+                user.test_data.update(environment.host, environment.parsed_options)
+                test_data[user.__class__.__name__] = user.test_data.data.to_json()
 
+        logger.info("Test data is ready")
+        for i, worker in enumerate(environment.runner.clients):
+            environment.runner.send_message("test_data", (test_data, i), worker)
+            logger.info(f"Test data is sent to worker {i}")
 
-def on_test_start(environment, **_kwargs):
-    # It will be called for any runner (master, worker, local)
+        # Print master details to the log
+        logger.info(
+            f"Master: test_start.add_listener: The test is started, "
+            f"Environment: {environment.runner}",
+        )
+
     if not isinstance(environment.runner, MasterRunner):
         # Print worker details to the log
         logger.info(
@@ -53,12 +91,6 @@ def on_test_start(environment, **_kwargs):
             f"The test is started, Environment: {environment.runner}",
         )
         Timer.set_timer(environment.runner.worker_index)
-    else:
-        # Print master details to the log
-        logger.info(
-            f"Master: test_start.add_listener: The test is started, "
-            f"Environment: {environment.runner}",
-        )
 
 
 # Listener for the test stop event
