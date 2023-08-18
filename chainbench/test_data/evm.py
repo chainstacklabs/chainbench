c8b80670b9d0e5a6510961acb504cb5a145f05a9
Erwin Wee <erwin.wee@chainstack.com>
2023-08-18 16:58:33 +0800

    add BASE support
    - add base mainnet and testnet to test data
    - enable test to automatically pull recent blocks for test data if chain ID not defined in CHAIN_INFO
    - upgrade locust and click versions, and change psycopg2 to psycopg2-binary

--- chainbench/test_data/evm.py
+++ chainbench/test_data/evm.py
@@ -1,7 +1,10 @@
+import logging
 from typing import Mapping
 
 from chainbench.test_data.base import BaseTestData, BlockchainData, Blocks, ChainInfo
 from chainbench.util.rng import get_rng
+
+logger = logging.getLogger(__name__)
 
 
 class EVMTestData(BaseTestData):
@@ -35,6 +38,16 @@ class EVMTestData(BaseTestData):
             "start_block": 20000000,
             "end_block": 32000000,
         },
+        8453: {
+            "name": "base-mainnet",
+            "start_block": 1,
+            "end_block": 2400000,
+        },
+        84531: {
+            "name": "base-testnet",
+            "start_block": 1,
+            "end_block": 8200000,
+        },
     }
 
     def _fetch_chain_id(self) -> int:
@@ -57,23 +70,29 @@ class EVMTestData(BaseTestData):
         block_number = rng.random.randint(start, end)
         return self._fetch_block(block_number, return_txs=return_txs)
 
+    def _get_start_and_end_blocks(self, use_recent_blocks: bool) -> tuple[int, int]:
+        chain_id: int = self._fetch_chain_id()
+        if not use_recent_blocks and chain_id in self.CHAIN_INFO:
+            start_block_number = self.CHAIN_INFO[chain_id]["start_block"]
+            end_block_number = self.CHAIN_INFO[chain_id]["end_block"]
+            logger.info("Using blocks from %s to %s as test data", start_block_number, end_block_number)
+            return start_block_number, end_block_number
+        else:
+            end_block_number = int(self._make_call("eth_blockNumber"), 0)
+            start_block_number = end_block_number - 20
+            logger.info("Using recent blocks from %s to %s as test data", start_block_number, end_block_number)
+            return start_block_number, end_block_number
+
     # get initial data from blockchain
     def _get_init_data(self, parsed_options) -> BlockchainData:
         txs: list[dict] = []
         tx_hashes: list[str] = []
         accounts: set[str] = set()
         blocks: Blocks = []
-        chain_id: int = self._fetch_chain_id()
         start_block_number: int
         end_block_number: int
         return_txs: bool
-
-        if parsed_options.use_recent_blocks:
-            end_block_number = int(self._make_call("eth_blockNumber"), 0)
-            start_block_number = end_block_number - 20
-        else:
-            start_block_number = self.CHAIN_INFO[chain_id]["start_block"]
-            end_block_number = self.CHAIN_INFO[chain_id]["end_block"]
+        start_block_number, end_block_number = self._get_start_and_end_blocks(parsed_options.use_recent_blocks)
 
         while self.TXS_REQUIRED > len(txs) or self.ACCOUNTS_REQUIRED > len(accounts) or self.SAVE_BLOCKS > len(blocks):
             if self.ACCOUNTS_REQUIRED > len(accounts) or self.TXS_REQUIRED > len(txs):
