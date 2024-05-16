0e650b6df200e79805f3be18fd2eab28e59fc282
Erwin Wee <erwin.wee@chainstack.com>
2024-05-16 14:06:55 +0800

    revert temporary change to debug_trace profile

--- chainbench/profile/evm/debug_trace.py
+++ chainbench/profile/evm/debug_trace.py
@@ -12,18 +12,17 @@ class EvmDebugTraceProfile(EvmUser):
     wait_time = constant_pacing(10)
 
     rpc_calls = {
-        # EvmUser.debug_trace_transaction: 1229,
-        # EvmUser.trace_transaction: 484,
-        # EvmUser.trace_block: 143,
-        # EvmUser.debug_trace_call: 60,
-        EvmUser.trace_call: 1,
-        EvmUser.trace_call_many: 1,
-        EvmUser.trace_filter: 1,
-        # EvmUser.trace_replay_block_transactions: 21,
-        # EvmUser.trace_replay_transaction: 11,
-        # EvmUser.debug_trace_block_by_hash: 6,
-        # EvmUser.debug_trace_block_by_number: 6,
-        # EvmUser.debug_get_raw_receipts: 1,
+        EvmUser.debug_trace_transaction: 1229,
+        EvmUser.trace_transaction: 484,
+        EvmUser.trace_block: 143,
+        EvmUser.debug_trace_call: 60,
+        EvmUser.trace_call_many: 25,
+        EvmUser.trace_filter: 17,
+        EvmUser.trace_replay_block_transactions: 21,
+        EvmUser.trace_replay_transaction: 11,
+        EvmUser.debug_trace_block_by_hash: 6,
+        EvmUser.debug_trace_block_by_number: 6,
+        EvmUser.debug_get_raw_receipts: 1,
     }
 
     tasks = EvmUser.expand_tasks(rpc_calls)
