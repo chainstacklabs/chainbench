99b34ad44ce06c31a6f5dc674479762f31004edb
Erwin Wee <erwin.wee@chainstack.com>
2024-07-30 20:22:09 +0800

    Add Solana getProgramAccounts profiles

---
+++ chainbench/profile/solana/get_program_accounts_shark.py
@@ -0,0 +1,19 @@
+from locust import task
+
+from chainbench.user.protocol.solana import SolanaUser
+from chainbench.user.http import RpcCall
+
+
+class GetProgramAccounts(SolanaUser):
+    @task
+    def get_program_accounts_shark_task(self) -> None:
+        self.make_rpc_call(
+            RpcCall(
+                method="getProgramAccounts",
+                params=[
+                    "SharkXwkS3h24fJ2LZvgG5tPbsH3BKQYuAtKdqskf1f",
+                    {"encoding": "base64", "commitment": "confirmed"}
+                ],
+            ),
+            name="getProgramAccounts_shark"
+        )
