5892d4cbecf9a43716a2eb73c502af0798a02556
Adrian <adriantpaez@gmail.com>
2025-11-13 19:22:03 +0100

    feat: enhance SurgeProfile with additional RPC tasks for Ethereum

--- chainbench/profile/surge/general.py
+++ chainbench/profile/surge/general.py
@@ -7,11 +7,59 @@ class SurgeProfile(EvmUser):
     wait_time = constant_pacing(2)
 
     @task
+    def get_gas_price_task(self):
+        self.make_rpc_call(
+            name="eth_gasPrice",
+            method="eth_gasPrice",
+            params=[],
+        ),
+
+    @task
+    def get_chain_id_task(self):
+        self.make_rpc_call(
+            name="eth_chainId",
+            method="eth_chainId",
+            params=[],
+        ),
+
+    @task
+    def net_version_task(self):
+        self.make_rpc_call(
+            name="net_version",
+            method="net_version",
+            params=[],
+        ),
+
+    @task
+    def syncing_task(self):
+        self.make_rpc_call(
+            name="eth_syncing",
+            method="eth_syncing",
+            params=[],
+        ),
+    
+    @task
+    def get_max_priority_fee_per_gas_task(self):
+        self.make_rpc_call(
+            name="eth_maxPriorityFeePerGas",
+            method="eth_maxPriorityFeePerGas",
+            params=[],
+        ),
+
+    @task
+    def get_fee_history_task(self):
+        self.make_rpc_call(
+            name="eth_feeHistory",
+            method="eth_feeHistory",
+            params=["0x5", "latest", [50, 90]],
+        ),
+
+    @task
     def get_block_by_number_task(self):
         self.make_rpc_call(
             name="eth_getBlockByNumber",
             method="eth_getBlockByNumber",
-            params=["0xa", True],
+            params=["latest", True],
         ),
 
     @task
@@ -19,21 +67,29 @@ class SurgeProfile(EvmUser):
         self.make_rpc_call(
             name="eth_getBalance",
             method="eth_getBalance",
-            params=[self.test_data.get_random_account(get_rng()), "latest"],
+            params=["0x0742D35Cc6634c0532925A3b844bc9e7595f3574", "latest"],
         ),
     
     @task
-    def get_transaction_receipt_task(self):
+    def get_block_number_task(self):
         self.make_rpc_call(
-            name="eth_getTransactionReceipt",
-            method="eth_getTransactionReceipt",
-            params=[self.test_data.get_random_tx_hash(get_rng())],
+            name="eth_blockNumber",
+            method="eth_blockNumber",
+            params=[],
+        ),
+
+    @task
+    def get_block_receipts_task(self):
+        self.make_rpc_call(
+            name="eth_getBlockReceipts",
+            method="eth_getBlockReceipts",
+            params=["latest"],
         ),
     
     @task
-    def get_storage_at_task(self):
+    def get_block_transaction_count_by_number_task(self):
         self.make_rpc_call(
-            name="eth_getStorageAt",
-            method="eth_getStorageAt",
-            params=[self.test_data.get_random_account(get_rng()), "0x0", "latest"],
+            name="eth_getBlockTransactionCountByNumber",
+            method="eth_getBlockTransactionCountByNumber",
+            params=["latest"],
         ),
