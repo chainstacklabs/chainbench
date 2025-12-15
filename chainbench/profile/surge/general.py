32161b4928135b0564825b3446ab0b483f332e99
Adrian <adriantpaez@gmail.com>
2025-12-15 11:50:04 +0100

    refactor: clean up SurgeProfile RPC call formatting and update test network block number

--- chainbench/profile/surge/general.py
+++ chainbench/profile/surge/general.py
@@ -1,6 +1,5 @@
 from chainbench.user import EvmUser
-from chainbench.util.rng import get_rng
-from locust import task, constant_pacing
+from locust import constant_pacing, task
 
 
 class SurgeProfile(EvmUser):
@@ -8,35 +7,35 @@ class SurgeProfile(EvmUser):
 
     @task(35)
     def eth_call_task(self):
-         self.make_rpc_call(
-                name="eth_call",
-                method="eth_call",
-                params=[{"to": "0xE837861567a541EFbcf0a8A6C56aEb61dD7A2C20", "data": "0x20965255"}, "latest"],
-            ),
-    
+        self.make_rpc_call(
+            name="eth_call",
+            method="eth_call",
+            params=[{"to": "0xE837861567a541EFbcf0a8A6C56aEb61dD7A2C20", "data": "0x20965255"}, "latest"],
+        )
+
     @task(15)
     def get_logs_task(self):
         self.make_rpc_call(
             name="eth_getLogs",
             method="eth_getLogs",
             params=self._get_logs_params_factory(self.rng.get_rng()),
-        ),
-    
+        )
+
     @task(5)
     def estimate_gas_task(self):
         self.make_rpc_call(
             name="eth_estimateGas",
             method="eth_estimateGas",
             params=[{"to": "0xE837861567a541EFbcf0a8A6C56aEb61dD7A2C20", "data": "0x20965255"}],
-        ),
-    
+        )
+
     @task(10)
     def get_block_by_number_task(self):
         self.make_rpc_call(
             name="eth_getBlockByNumber",
             method="eth_getBlockByNumber",
             params=self._block_params_factory(),
-        ),
+        )
 
     @task(8)
     def get_balance_task(self):
@@ -44,7 +43,7 @@ class SurgeProfile(EvmUser):
             name="eth_getBalance",
             method="eth_getBalance",
             params=["0x0742D35Cc6634c0532925A3b844bc9e7595f3574", "latest"],
-        ),
+        )
 
     @task(7)
     def get_transaction_receipt_task(self):
@@ -52,7 +51,7 @@ class SurgeProfile(EvmUser):
             name="eth_getTransactionReceipt",
             method="eth_getTransactionReceipt",
             params=self._transaction_by_hash_params_factory(self.rng.get_rng()),
-        ),
+        )
 
     @task(5)
     def get_storage_at_task(self):
@@ -60,7 +59,7 @@ class SurgeProfile(EvmUser):
             name="eth_getStorageAt",
             method="eth_getStorageAt",
             params=["0x0742D35Cc6634c0532925A3b844bc9e7595f3574", "0x0", "latest"],
-        ),
+        )
 
     @task(5)
     def get_gas_price_task(self):
@@ -68,7 +67,7 @@ class SurgeProfile(EvmUser):
             name="eth_gasPrice",
             method="eth_gasPrice",
             params=[],
-        ),
+        )
 
     @task(2)
     def get_chain_id_task(self):
@@ -76,7 +75,7 @@ class SurgeProfile(EvmUser):
             name="eth_chainId",
             method="eth_chainId",
             params=[],
-        ),
+        )
 
     @task(1)
     def net_version_task(self):
@@ -84,7 +83,7 @@ class SurgeProfile(EvmUser):
             name="net_version",
             method="net_version",
             params=[],
-        ),
+        )
 
     @task(1)
     def syncing_task(self):
@@ -92,15 +91,15 @@ class SurgeProfile(EvmUser):
             name="eth_syncing",
             method="eth_syncing",
             params=[],
-        ),
-    
+        )
+
     @task(1)
     def get_max_priority_fee_per_gas_task(self):
         self.make_rpc_call(
             name="eth_maxPriorityFeePerGas",
             method="eth_maxPriorityFeePerGas",
             params=[],
-        ),
+        )
 
     @task(2)
     def get_fee_history_task(self):
@@ -108,7 +107,7 @@ class SurgeProfile(EvmUser):
             name="eth_feeHistory",
             method="eth_feeHistory",
             params=["0x5", "latest", [50, 90]],
-        ),
+        )
 
     @task(1)
     def get_block_number_task(self):
@@ -116,7 +115,7 @@ class SurgeProfile(EvmUser):
             name="eth_blockNumber",
             method="eth_blockNumber",
             params=[],
-        ),
+        )
 
     @task(1)
     def get_block_receipts_task(self):
@@ -124,12 +123,12 @@ class SurgeProfile(EvmUser):
             name="eth_getBlockReceipts",
             method="eth_getBlockReceipts",
             params=["latest"],
-        ),
-    
+        )
+
     @task(1)
     def get_block_transaction_count_by_number_task(self):
         self.make_rpc_call(
             name="eth_getBlockTransactionCountByNumber",
             method="eth_getBlockTransactionCountByNumber",
             params=["latest"],
-        ),
+        )
